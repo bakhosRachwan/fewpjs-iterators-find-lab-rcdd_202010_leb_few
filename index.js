d945db7658d779d666198567198641dfde31aad2
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-08 07:53:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ function testFunc() {
 }
 
 const superBowlWin = (record) => {
-  let win = record.find(x => {
-    return x.result === "W"
-  })
-  return !!win? win.year : undefined
+  let win = record.find(x => x.result === "W")
+  return !!win ? win.year : undefined
 }
